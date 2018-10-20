c4d2e03833cac67be66ff5814ac1ce5241c2b436
Benjamin Ashbaugh <ashbau.benjam19@svvsd.org>
2018-10-20 16:58:00 -0600

    started RNN v1 notebook

--- MLtest/boston.py
+++
@@ -1,9 +0,0 @@
-from keras.datasets import boston_housing
-(traind, traint), (testd, testt) = boston_housing.load_data()
-mean = traind.mean(axis=0)
-traind -= mean
-std = traind.std(axis=0)
-traind /= std
-testd -= mean
-testd /= std
-
